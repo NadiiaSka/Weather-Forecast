be116376d9c6499c7e57f1a1e8c99fb702528bf9
NadiiaSka <syvakivska@gmail.com>
2024-02-18 18:09:07 +0700

    improve forecast details styling

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -99,27 +99,27 @@ const Forecast = ({ data }) => {
               <AccordionItemPanel>
                 <div className="daily-details-grid">
                   <div className="daily-details-grid-item">
-                    <label>Pressure</label>
+                    <label>Pressure:</label>
                     <label>{item.main.pressure} hPa</label>
                   </div>
                   <div className="daily-details-grid-item">
-                    <label>Humidity</label>
+                    <label>Humidity:</label>
                     <label>{item.main.humidity}%</label>
                   </div>
                   <div className="daily-details-grid-item">
-                    <label>Clouds</label>
+                    <label>Clouds:</label>
                     <label>{item.clouds.all}%</label>
                   </div>
                   <div className="daily-details-grid-item">
-                    <label>Wind speed</label>
+                    <label>Wind speed:</label>
                     <label>{item.wind.speed} m/s</label>
                   </div>
                   <div className="daily-details-grid-item">
-                    <label>Sea level</label>
+                    <label>Sea level:</label>
                     <label>{item.main.sea_level} m</label>
                   </div>
                   <div className="daily-details-grid-item">
-                    <label>Feels like</label>
+                    <label>Feels like:</label>
                     <label>{Math.round(item.main.feels_like)}°</label>
                   </div>
                 </div>
