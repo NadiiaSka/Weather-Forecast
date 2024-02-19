089db974eb51e8521bcbbe61bbc67c5789b250ae
NadiiaSka <syvakivska@gmail.com>
2024-02-19 12:53:55 +0700

    refactor to use axios to query

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
-export const geoApiOptions = {
+import axios from "axios";
+
+const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+const geoApiOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "03531e4774mshc97d75550a5d7d6p1d3136jsn2b02678ced8f",
@@ -7,5 +9,46 @@ export const geoApiOptions = {
   },
 };
 
+export const cityOptions = async (inputValue) => {
+  try {
+    const response = await axios.get(`${GEO_API_URL}`, {
+      params: {
+        minPopulation: 1000000,
+        namePrefix: inputValue,
+      },
+      ...geoApiOptions,
+    });
+    const options = response.data.data.map((city) => ({
+      value: `${city.latitude} ${city.longitude}`,
+      label: `${city.city}, ${city.countryCode}`,
+    }));
+    return {
+      options,
+      hasMore: false,
+    };
+  } catch (error) {
+    console.error(error);
+    return { options: [], hasMore: false };
+  }
+};
+
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "a9c5f47a2a143ab0745e760d11f4c41c";
+
+export const fetchCurrentLocation = async () => {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          resolve({ latitude, longitude });
+        },
+        (error) => {
+          reject(error.message);
+        }
+      );
+    } else {
+      reject("Geolocation is not supported by this browser.");
+    }
+  });
+};
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from "../../api";
+import { cityOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
@@ -10,34 +10,14 @@ const Search = ({ onSearchChange }) => {
     onSearchChange(searchData);
   };
 
-  const loadOptions = async (inputValue) => {
-    try {
-      const response = await fetch(
-        `${GEO_API_URL}?minPopulation=1000000&namePrefix=${inputValue}`,
-        geoApiOptions
-      );
-      const result = await response.json();
-      const formatedResult = {
-        options: result.data.map((city) => {
-          return {
-            value: `${city.latitude} ${city.longitude}`,
-            label: `${city.city}, ${city.countryCode}`,
-          };
-        }),
-      };
-      return formatedResult;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <AsyncPaginate
       placeholder="Search for city"
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
-      loadOptions={loadOptions}
+      loadOptions={cityOptions}
+      isSearchable
     />
   );
 };
